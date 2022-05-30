106048294e7eed963a48ff2d342aee61aa4e0cc1
Gleb Son <57506521+glebs0n1@users.noreply.github.com>
2022-05-30 14:39:47 +0300

    Update BankServiceApplicationTests.java

--- BankService/src/test/java/com/example/bankservice/BankServiceApplicationTests.java
+++ BankService/src/test/java/com/example/bankservice/BankServiceApplicationTests.java
@@ -2,7 +2,6 @@ package com.example.bankservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class BankServiceApplicationTests {
 
